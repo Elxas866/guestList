fe7a3ba1fb52eafd9e0f6bbe0d88433441c9bd22
Elias Kelta <eliaskelta1@gmail.com>
2024-06-16 08:48:19 +0200

    reinitialized frontend as no-standalone

--- frontend/guestlist_frontent/src/app/app.component.spec.ts
+++ frontend/guestlist_frontent/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'guestlist_frontend' title`, () => {
+  it(`should have as title 'guestlist_frontent'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('guestlist_frontend');
+    expect(app.title).toEqual('guestlist_frontent');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, guestlist_frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, guestlist_frontent');
   });
 });
